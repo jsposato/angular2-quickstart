d4f07827879e27be25b74de8566a1e1266ff243a
John Sposato Jr <jsposato@sposatoenterprises.com>
2017-06-11 16:29:45 -0400

    update name on enter press.  Need to look at ngModel on an input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'app/app.component.html'
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'John';
   title = 'Customer App';
   wardsColor = 'blue';
 
